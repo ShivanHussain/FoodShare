de3a52f928862a7d35d29cca39926a91794c92fa
ShivanHussain <shivanhussain6@gmail.com>
2025-08-30 22:57:59 +0530

    Update adminAnalyticsSlice.js
    Change the BASE_URL

--- frontend/src/redux/slices/adminAnalyticsSlice.js
+++ frontend/src/redux/slices/adminAnalyticsSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const API_BASE_URL = "http://localhost:4000/api/v1/admin";
+const API_BASE_URL = "https://foodshare-wwb9.onrender.com/api/v1/admin";
 
 // ========== Thunks ==========
 export const fetchDashboardStats = createAsyncThunk(
