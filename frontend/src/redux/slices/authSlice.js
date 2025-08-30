b05a910af9b242ab7eae5e577f535b9cbda1c057
ShivanHussain <shivanhussain6@gmail.com>
2025-08-30 22:58:58 +0530

    Update authSlice.js
    change the base url

--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const BASE_URL = 'http://localhost:4000/api/v1/auth';
+const BASE_URL = 'https://foodshare-wwb9.onrender.com/api/v1/auth';
 
 // REGISTER  
 export const registerUser = createAsyncThunk(
