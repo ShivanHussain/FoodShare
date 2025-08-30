04028e47646d981701d0544a0e3fcf0dd16c9eb8
ShivanHussain <shivanhussain6@gmail.com>
2025-08-30 23:05:22 +0530

    Update userSlice.js
    change the base url

--- frontend/src/redux/slices/userSlice.js
+++ frontend/src/redux/slices/userSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const BASE_URL = 'http://localhost:4000/api/v1/user';
+const BASE_URL = 'https://foodshare-wwb9.onrender.com/api/v1/user';
 
 // ==============================
 // Async Thunks
